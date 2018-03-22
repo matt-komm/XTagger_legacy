ac0701ea08507758a0b91c248a5776fc067b7c0c
matt-komm <Matthias.Komm@cern.ch>
2018-03-22 17:06:32 +0000

    add missing packages

--- Tests/Training/AnacondaEnv/testSetupCpuEnv.sh
+++ Tests/Training/AnacondaEnv/testSetupCpuEnv.sh
@@ -1,8 +1,9 @@
 
 function execute() 
 {
-    ./Training/AnacondaEnv/setupEnv_cpuonly.sh testenv || return 1
+    ./Training/AnacondaEnv/setupEnv_cpuonly.sh Training/AnacondaEnv/testenv || return 1
     source Training/AnacondaEnv/env.sh || return 1
+    source activate tf_cpu || return 1
     python -c "import tensorflow as tf"
     python -c "import keras"
 }
