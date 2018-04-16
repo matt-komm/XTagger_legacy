6a05e0e2bc4d996349aa3bd850e4ef5a9bba5dad
matt-komm <Matthias.Komm@cern.ch>
2018-04-16 16:12:47 +0100

    up

--- Tests/Training/AnacondaEnv/testSetupCpuEnv.sh
+++ Tests/Training/AnacondaEnv/testSetupCpuEnv.sh
@@ -2,7 +2,7 @@
 function execute() 
 {
     source Training/AnacondaEnv/setupEnv_cpuonly.sh "Training/AnacondaEnv/testenv" || return 1
-    source Training/AnacondaEnv/env.sh || return 1
+    source Training/AnacondaEnv/env_cpu.sh || return 1
     source activate tf_cpu || return 1
     python -c "import tensorflow as tf" || return 1
     python -c "import keras" || return 1
--- Tests/Training/Ops/buildOps.sh
+++ Tests/Training/Ops/buildOps.sh
@@ -1,8 +1,7 @@
 
 function execute() 
 {
-    source Training/AnacondaEnv/env.sh || return 1
-    source activate tf_cpu || return 1
+    source Training/AnacondaEnv/env_cpu.sh || return 1
     mkdir Training/Ops/build || return 1
     cd Training/Ops/build || return 1
     cmake .. || return 1
