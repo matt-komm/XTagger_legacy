58407c512106ad7052f3f39449787e983ea97c04
matt-komm <Matthias.Komm@cern.ch>
2018-04-17 13:52:31 +0100

    add deactivation step

--- Training/AnacondaEnv/setupEnv_cpuonly.sh
+++ Training/AnacondaEnv/setupEnv_cpuonly.sh
@@ -77,6 +77,8 @@ function run_setup()
     echo "Installing packages"
     conda install -c nlesc root-numpy=4.4.0 --yes || return 1
     conda install -c conda-forge boost=1.64.0 --yes || return 1
+    source deactivate || return 1
+    source activate tf_cpu || return 1
     pip install --no-cache-dir -r $SCRIPT_DIR/packages_cpu.pip || return 1
     
     echo "export PATH="$INSTALL_ABSDIR"/miniconda/bin:\$PATH" > $SCRIPT_DIR/env_cpu.sh
