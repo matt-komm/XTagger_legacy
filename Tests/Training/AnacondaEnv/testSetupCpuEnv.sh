dbfb8e4d4b2c0f3cd70fc29d3b70f4fe40df1e97
matt-komm <Matthias.Komm@cern.ch>
2018-04-24 17:48:16 +0100

    caching output

--- Tests/Training/AnacondaEnv/testSetupCpuEnv.sh
+++ Tests/Training/AnacondaEnv/testSetupCpuEnv.sh
@@ -1,7 +1,14 @@
 
 function execute() 
 {
-    source Training/AnacondaEnv/setupEnv_cpuonly.sh "testenv" || return 1
+    source Training/AnacondaEnv/setupEnv_cpuonly.sh "testenv" &> env.build
+    if [ $? -eq 0 ]
+    then
+      echo "Successfully setup environment"
+    else
+      tail -n 500 env.build
+      return 1
+    fi
     source Training/AnacondaEnv/env_cpu.sh || return 1
     source activate tf_cpu || return 1
     python Tests/Training/AnacondaEnv/testKeras.py || return 1
