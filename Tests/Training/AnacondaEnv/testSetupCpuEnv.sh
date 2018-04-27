685a6b42cf59a39c454baa045b4f883d9513e331
matt-komm <Matthias.Komm@cern.ch>
2018-04-27 15:35:18 +0100

    fix

--- Tests/Training/AnacondaEnv/testSetupCpuEnv.sh
+++ Tests/Training/AnacondaEnv/testSetupCpuEnv.sh
@@ -1,15 +1,8 @@
+#!/bin/bash
 
 function execute() 
 {
-    source Training/AnacondaEnv/setupEnv_cpuonly.sh "testenv"  2>&1 | tee env.build | grep STATUS
-    if [ $? -eq 0 ]
-    then
-      echo "Successfully setup environment"
-    else
-      tail -n 300 env.build
-      return 1
-    fi
-    ls -lh
+    source Training/AnacondaEnv/setupEnv_cpuonly.sh "testenv"
     source env_cpu.sh || return 1
     source activate tf_cpu || return 1
     python Tests/Training/AnacondaEnv/testKeras.py || return 1
--- Training/AnacondaEnv/setupEnv_cpuonly.sh
+++ Training/AnacondaEnv/setupEnv_cpuonly.sh
@@ -3,26 +3,14 @@
 SCRIPT_DIR=`dirname ${BASH_SOURCE[0]}`
 STOP=""
 
-function execute() 
-{
-    if [ -z "$STOP" ]; then
-        echo -n $@ " ... "
-        out=$($@ 2>&1)
-        ret=$?
-
-        if [ $ret -eq 0 ]; then
-            echo "ok"
-        else
-            echo "error"
-            STOP=$out
-            return 1
-        fi
-    fi
-}
+LOGFILE=$SCRIPT_DIR/env.log
+echo "Setup run at "`date` > $LOGFILE
+echo "STATUS: Create log file "$LOGFILE
 
 function run_setup()
 {
     echo "STATUS: Script directory "$SCRIPT_DIR
+    
     if [[  -z  $1  ]] ; then
         echo 'Usage:'
         echo '  setupEnv_cpuonly.sh <install_dir>'
@@ -44,9 +32,12 @@ function run_setup()
         echo "Error - directory "$INSTALL_DIR" exists!"
         return 1
     fi
-    echo "STATUS: Setting up central environment under "$INSTALL_DIR
+    echo "Setting up central environment under "$INSTALL_DIR
+    
 
     execute mkdir $INSTALL_DIR || return 1
+    
+    
     
     INSTALL_ABSDIR=`readlink -e $INSTALL_DIR`
 
@@ -55,8 +46,8 @@ function run_setup()
         return 1
     fi
 
-    execute wget -P $INSTALL_ABSDIR https://repo.continuum.io/miniconda/Miniconda2-4.3.31-Linux-x86_64.sh || return 1
-    execute bash $INSTALL_ABSDIR/Miniconda2-4.3.31-Linux-x86_64.sh -b -s -p $INSTALL_ABSDIR/miniconda || return 1
+    execute wget -P $INSTALL_ABSDIR https://repo.continuum.io/miniconda/Miniconda2-4.3.31-Linux-x86_64.sh &>> $LOGFILE || return 1
+    execute bash $INSTALL_ABSDIR/Miniconda2-4.3.31-Linux-x86_64.sh -b -s -p $INSTALL_ABSDIR/miniconda &>> $LOGFILE || return 1
 
     CONDA_BIN=$INSTALL_ABSDIR/miniconda/bin
     export PATH=$CONDA_BIN:$PATH
@@ -69,33 +60,40 @@ function run_setup()
     mkdir $TMPDIR
    
     
-    echo "STATUS: Create environment for CPU"
+    echo "Create environment for CPU"
     #rm -f /tmp/*
     
-    conda create -n tf_cpu python=2.7 --yes || return 1
-    source activate tf_cpu || return 1
+    conda create -n tf_cpu python=2.7 --yes &>> $LOGFILE || return 1
+    source activate tf_cpu &>> $LOGFILE || return 1
     
     #pip install tensorflow==1.6.0 || return 1
     #conda install -c conda-forge cmake --yes || return 1
     #conda install -c nlesc root-numpy=4.4.0 --yes || return 1
     #conda install -c conda-forge boost=1.64.0 --yes || return 1
-    echo "STATUS: Installing root"
-    conda install -c nlesc root-numpy=4.4.0 --yes || return 1
-    echo "STATUS: Installing pip packages"
-    pip install --no-cache-dir -r $SCRIPT_DIR/packages_cpu.pip || return 1
+    echo "Installing root"
+    conda install -c nlesc root-numpy=4.4.0 --yes &>> $LOGFILE || return 1
+    echo "Installing pip packages"
+    pip install --no-cache-dir -r $SCRIPT_DIR/packages_cpu.pip &>> $LOGFILE || return 1
     
+    echo "Generate setup script"
     echo "export PATH="$INSTALL_ABSDIR"/miniconda/bin:\$PATH" > env_cpu.sh
     #echo "export LD_PRELOAD="$INSTALL_ABSDIR"/miniconda/lib/libmkl_core.so:"$INSTALL_ABSDIR"/miniconda/lib/libmkl_sequential.so:\$LD_PRELOAD" >> $SCRIPT_DIR/env_cpu.sh
     echo "source activate tf_cpu" >> env_cpu.sh
 
-    echo "STATUS: Create script for environment: env_cpu.sh "`ll env_cpu.sh`
+    echo "Create script for environment: env_cpu.sh "`ll env_cpu.sh`
 
-    source deactivate || return 1
+    source deactivate &>> $LOGFILE || return 1
     
-    rm -rf $INSTALL_ABSDIR/tmp
+    rm -rf $INSTALL_ABSDIR/tmp &>> $LOGFILE
 
 }
 
 run_setup $1
-
+if [ $? -eq 0 ]
+then
+  echo "Successfully setup environment"
+else
+  tail -n 300 $LOGFILE
+  return 1
+fi
 
